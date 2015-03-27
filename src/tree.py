738383bf7b777d85691307cf3ca3b17b3cf7bf55
jianming Zhou <diymyfuture@gmail.com>
2015-03-27 16:29:06 -0400

    tree with graph

--- src/tree.py
+++ src/tree.py
@@ -14,7 +14,11 @@ print fitTree.predict([1.,2.,1.,4.])
 #print the corresponding probabilities
 print fitTree.predict_proba([1.,2.,1.,4.])#prediction with prob
 
-
+## visualized the tree 
 from sklearn.externals.six import StringIO
 import pydot
 dot_data=StringIO()
+out=tree.export_graphviz(fitTree,out_file=dot_data)
+treeGraph=pydot.graph_from_dot_data(dot_data.getvalue())
+# writing to pdf file 
+treeGraph.write_pdf("tree.pdf") 
